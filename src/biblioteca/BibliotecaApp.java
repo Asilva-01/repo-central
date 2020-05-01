8bcd7263b86c5fe5c03e1f11cca2382b389bfbd4
André <andre1981luiz@gmail.com>
2020-05-01 18:22:55 -0300

    Criação do Livro

--- src/biblioteca/BibliotecaApp.java
+++ src/biblioteca/BibliotecaApp.java
@@ -5,6 +5,8 @@ public class BibliotecaApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Livro 1 = new Livro();
+		
 	}
 
 }
---
+++ src/biblioteca/Livro.java
@@ -0,0 +1,7 @@
+package biblioteca;
+
+public class Livro {
+
+	private String titulo;  
+}
+
